c54985c837bd94c4b21333b1e50ae631e7c052e7
kimmatches <peach3139@gmail.com>
2024-09-25 14:11:42 +0900

    영수증 설명 위치 고정 수정

--- src/component/Receipt.jsx
+++ src/component/Receipt.jsx
@@ -5,20 +5,24 @@ const Receipt = ({ children, explanation }) => {
   return (
     <div className="receipt">
       <h1 className='app-name'>PayDay</h1>
-      {explanation && <p className='explanation'>{explanation}</p>}
-      <hr></hr>
+      <p className='explanation' style={{ visibility: explanation ? 'visible' : 'hidden' }}>
+        {explanation || ' '}
+      </p>
+      <hr />
       {children}
     </div>
   );
 };
 
 Receipt.propTypes = {
-    children: PropTypes.node, 
+    children: PropTypes.node,
     explanation: PropTypes.string,
-  };
-  Receipt.defaultProps = {
+};
+
+Receipt.defaultProps = {
     explanation: null,
-  };
+};
 
 export default Receipt;
+
 
